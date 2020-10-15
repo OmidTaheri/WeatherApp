741038ec919cb9c882a5d0d830043beccc426444
OmidTaheri <m.omidtaheri@yahoo.com>
2020-10-15 11:46:11 +0330

    config buildSrc module instance of gradle

--- buildSrc/src/main/java/BuildModules.kt
+++ buildSrc/src/main/java/BuildModules.kt
@@ -1,20 +1,13 @@
 object BuildModules {
     const val app = ":app"
     const val DaggerCore = ":DaggerCore"
-    const val GenreList = ":GenreList"
     const val ViewComponents = ":ViewComponents"
     const val UiBase = ":UiBase"
     const val AndroidBase = ":AndroidBase"
-    const val searchtv = ":searchtv"
-    const val mainpagetv = ":mainpagetv"
-    const val Logging = ":Logging"
     const val Remote = ":Remote"
-    const val Local = ":Local"
+    const val SharedPref = ":SharedPref"
     const val Domain = ":Domain"
     const val Data = ":Data"
-    const val Favorite = ":Favorite"
-    const val Search = ":Search"
     const val MainPage = ":MainPage"
-    const val tvapp = ":tvapp"
 
 }
