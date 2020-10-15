994f8185a24fb9f1f2b40c0907872442a596f669
OmidTaheri <m.omidtaheri@yahoo.com>
2020-10-15 11:49:09 +0330

    add  ViewComponents module (custom multistate Ui components)

---
+++ ViewComponents/build.gradle.kts
@@ -0,0 +1,81 @@
+
+import dependencies.Dependencies
+import dependencies.UiDependencies
+import extentions.addTestsDependencies
+import extentions.implementation
+
+plugins {
+    id(BuildPlugins.ANDROID_LIBRARY)
+    kotlin(BuildPlugins.KOTLIN_ANDROID)
+    kotlin(BuildPlugins.KOTLIN_ANDROID_EXTENSIONS)
+    id(BuildPlugins.KOTLIN_KAPT)
+}
+
+android {
+    compileSdkVersion(BuildAndroidConfig.COMPILE_SDK_VERSION)
+    buildToolsVersion(BuildAndroidConfig.BUILD_TOOLS_VERSION)
+
+    defaultConfig {
+        minSdkVersion(BuildAndroidConfig.MIN_SDK_VERSION)
+        targetSdkVersion(BuildAndroidConfig.TARGET_SDK_VERSION)
+        versionCode = BuildAndroidConfig.VERSION_CODE
+        versionName = BuildAndroidConfig.VERSION_NAME
+
+        vectorDrawables.useSupportLibrary = BuildAndroidConfig.SUPPORT_LIBRARY_VECTOR_DRAWABLES
+        testInstrumentationRunner = BuildAndroidConfig.TEST_INSTRUMENTATION_RUNNER
+    }
+
+    buildTypes {
+        getByName(BuildTypes.RELEASE) {
+            isMinifyEnabled = BuildTypeRelease.isMinifyEnabled
+            isDebuggable = BuildTypeRelease.debuggable
+            isTestCoverageEnabled = BuildTypeRelease.isTestCoverageEnabled
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        getByName(BuildTypes.DEBUG) {
+            isMinifyEnabled = BuildTypeDebug.isMinifyEnabled
+            isDebuggable = BuildTypeDebug.debuggable
+            isTestCoverageEnabled = BuildTypeDebug.isTestCoverageEnabled
+        }
+    }
+
+    flavorDimensions(BuildProductDimensions.BASEDIMENT)
+
+    productFlavors {
+        FullFlavor.libraryCreate(this)
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(Dependencies.KOTLIN)
+    implementation(Dependencies.CORE_KTX)
+    implementation(UiDependencies.APPCOMPAT)
+
+    implementation(UiDependencies.CONSTRAINT_LAYOUT)
+    implementation(UiDependencies.RECYCLE_VIEW)
+    implementation(UiDependencies.SWIPE_REFRESH_LAYOUT)
+
+    implementation(project(mapOf("path" to BuildModules.UiBase)))
+
+    addTestsDependencies()
+}
