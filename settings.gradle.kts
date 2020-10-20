f4e3213c3cd9035412fbff7cd8a4a383f11b758a
OmidTaheri <m.omidtaheri@yahoo.com>
2020-10-20 20:09:54 +0330

    settings file by kotlin DSL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
-include (":app")
+include (":SharedPref")
+include (":MainPage")
+include (":DaggerCore")
+include (":ViewComponents")
+include (":UiBase")
+include (":AndroidBase")
+include (":Remote")
+include (":Data")
+include(":app")
+include(":Domain")
 rootProject.name = "WeatherApp"
 rootProject.buildFileName = "build.gradle.kts"
