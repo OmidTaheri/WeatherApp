7b95ee7ecde203b84abf7b43d635eecd349ad813
OmidTaheri <m.omidtaheri@yahoo.com>
2020-10-20 20:11:19 +0330

    change ApplicationId

--- buildSrc/src/main/java/BuildAndroidConfig.kt
+++ buildSrc/src/main/java/BuildAndroidConfig.kt
@@ -1,12 +1,10 @@
 object BuildAndroidConfig {
 
-    const val APPLICATION_ID = "ir.omidtaheri.movotlin"
-    const val TV_APPLICATION_ID = "ir.omidtaheri.movotlintv"
+    const val APPLICATION_ID = "ir.omidtaheri.weatherapp"
 
     const val BUILD_TOOLS_VERSION = "29.0.3"
     const val COMPILE_SDK_VERSION =29
     const val MIN_SDK_VERSION =21
-    const val TV_MIN_SDK_VERSION =21
     const val TARGET_SDK_VERSION =29
 
     const val VERSION_CODE = 1
