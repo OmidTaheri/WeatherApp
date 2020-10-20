2ba2d20b59f1c88ddb7c2ef8200a0d35f519b172
OmidTaheri <m.omidtaheri@yahoo.com>
2020-10-20 20:05:28 +0330

    delete unused files

--- Data/src/main/java/ir/omidtaheri/data/datasource/local/MovieLocalDataSourceInterface.kt
+++
@@ -1,8 +0,0 @@
-package ir.omidtaheri.data.datasource.local
-
-interface MovieLocalDataSourceInterface {
-//    fun favoriteMovie(movie: FavoritedMovieDataEntity): Single<Long>
-//    fun unFavoriteMovie(movie: FavoritedMovieDataEntity): Single<Int>
-//    fun getFavoritedMoviesList(): Observable<List<FavoritedMovieDataEntity>>
-//    fun getFavoritedMoviesListByFlowable(): Flowable<List<FavoritedMovieDataEntity>>
-}
