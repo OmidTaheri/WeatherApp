e6a741cd1e824f8441aa8a83af71a1edec1d0c74
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-09-25 12:45:33 +0330

    Refactor BaseViewModel to viewModel and base on lint warning

--- AndroidBase/src/main/java/ir/omidtaheri/androidbase/BaseViewModel.kt
+++ AndroidBase/src/main/java/ir/omidtaheri/androidbase/BaseViewModel.kt
@@ -1,49 +1,36 @@
 package ir.omidtaheri.androidbase
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import ir.omidtaheri.androidbase.singleLiveData.SingleLiveData
 
-open class BaseViewModel(application: Application) : AndroidViewModel(application) {
-    val ApplicationClass: Application
+open class BaseViewModel() : ViewModel() {
 
-    init {
-        ApplicationClass = application
-    }
+    protected val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
-    protected val _isLoading: MutableLiveData<Boolean>
+    protected val _isLoading: MutableLiveData<Boolean> = MutableLiveData()
     val isLoading: LiveData<Boolean>
         get() = _isLoading
 
-    protected val _ErrorSnackBar: SingleLiveData<String>
-    val ErrorSnackBar: LiveData<String>
-        get() = _ErrorSnackBar
+    protected val _errorSnackBar: SingleLiveData<String> = SingleLiveData()
+    val errorSnackBar: LiveData<String>
+        get() = _errorSnackBar
 
-    protected val _ErrorToast: SingleLiveData<String>
-    val ErrorToast: LiveData<String>
-        get() = _ErrorToast
+    protected val _errorToast: SingleLiveData<String> = SingleLiveData()
+    val errorToast: LiveData<String>
+        get() = _errorToast
 
-    protected val _MessageSnackBar: SingleLiveData<String>
-    val MessageSnackBar: LiveData<String>
-        get() = _MessageSnackBar
+    protected val _messageSnackBar: SingleLiveData<String> = SingleLiveData()
+    val messageSnackBar: LiveData<String>
+        get() = _messageSnackBar
 
-    protected val _MessageToast: SingleLiveData<String>
-    val MessageToast: LiveData<String>
-        get() = _MessageToast
+    protected val _messageToast: SingleLiveData<String> = SingleLiveData()
+    val messageToast: LiveData<String>
+        get() = _messageToast
 
-    init {
-        _isLoading = MutableLiveData()
-        _ErrorSnackBar = SingleLiveData()
-        _ErrorToast = SingleLiveData()
-        _MessageSnackBar = SingleLiveData()
-        _MessageToast = SingleLiveData()
-    }
-
-    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     fun addDisposable(disposable: Disposable) {
 
@@ -54,7 +41,6 @@ open class BaseViewModel(application: Application) : AndroidViewModel(applicatio
 
         compositeDisposable.delete(disposable)
     }
-
 
     private fun clearDisposables() {
         compositeDisposable.clear()
